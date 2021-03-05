a4769f3abdcc3d595f594deb1bd9442c9b7c8e94
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-03-05 12:24:32 -0300

    robo de teste por comportamento

---
+++ cypress/integration/cliente.spec.js
@@ -0,0 +1,27 @@
+context('testar Home Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Deve preencher um CPF e verificar como válido', () => {
+    cy.get('input[name="nome"]').type('Danilo')
+    cy.get('input[name="cpf"]').type('351.455.030-16')
+    // cy.wait(1000)
+    cy.get('form button').click()
+    // cy.wait(1000)
+    cy.get('.alert-success').should("to.have.length", 1)
+    cy.get('.alert-danger').should("to.have.length", 0)
+    // cy.wait(1000)
+  })
+
+  it('Deve preencher um CPF e verificar como inválido', () => {
+    cy.get('input[name="nome"]').type('Danilo')
+    cy.get('input[name="cpf"]').type('351.455.030-26')
+    // cy.wait(1000)
+    cy.get('form button').click()
+    // cy.wait(1000)
+    cy.get('.alert-success').should("to.have.length", 0)
+    cy.get('.alert-danger').should("to.have.length", 1)
+    // cy.wait(1000)
+  })
+})
